3e555c08e4dae85db2a474dd0236c97381be5336
Aditi3731 <65819863+Aditi3731@users.noreply.github.com>
2020-10-19 19:51:13 +0530

    Update enterinteger.c

--- enterinteger.c
+++ enterinteger.c
@@ -2,9 +2,11 @@
 int main()
 {
     int number;
+    printf("status:\n");
     printf("Enter an integer:");
      scanf("%d",&number)
     printf("You entered: %d", number);
+    printf("\nworking!");
     return 0;
 }
 
